a126a81286ca8ae9ffd8d2f602de4af716774177
Betty <betty@Bettys-MacBook-Air.local>
2022-11-15 21:39:38 -0600

    Assignment7

--- assignment3/src/main/resources/data.sql
+++ assignment3/src/main/resources/data.sql
@@ -10,16 +10,16 @@ VALUES (1236, 'Mohamed');
 
 -- Posts data
 INSERT into POST (id, title, content, author, USER_ID)
-VALUES (1, 'first post','Life is structured in layers','Joe', 1234);
+VALUES (1, 'First post','Life is structured in layers','Joe', 1234);
 
 INSERT into POST (id, title, content, author, USER_ID)
-VALUES (2, 'second post','React is a good SPA library','John', 1234);
+VALUES (2, 'Second post','React is a good SPA library','John', 1234);
 
 INSERT into POST (id, title, content, author, USER_ID)
-VALUES (3, 'third','Spring is awesome','Luwam',1235);
+VALUES (3, 'Third','Spring is awesome','Luwam',1235);
 
 INSERT into POST (id, title, content, author, USER_ID)
-VALUES (4, 'fourth post','This is a module that works over spring modules','Ahmed',1236);
+VALUES (4, 'Fourth post','This is a module that works over spring modules','Ahmed',1236);
 
 
 -- Comments data
